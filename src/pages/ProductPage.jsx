4f5317814f12c415871588f3b6b8355b1f947eac
Soiyeah <sohanchz@gmail.com>
2021-10-19 23:22:35 +0530

    Implemented quantity selector functionality

--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -4,6 +4,7 @@ import {useState,useEffect} from 'react';
 import styled from "styled-components";
 import { Colors } from "../helper/Colors";
 import Navbar from "../components/Navbar";
+import { Button } from "@material-ui/core";
 
 const Container = styled.div`
     width: 100%;
@@ -14,7 +15,7 @@ const Wrapper = styled.div`
     border: ${Colors.lightGray} 1px solid;
     border-radius: 10px;
     display: flex;
-    margin: 10%;
+    margin: 5%;
 `;
 
 const ImagesPane = styled.div`
@@ -22,9 +23,17 @@ const ImagesPane = styled.div`
     flex: 1;
 `;
 
+const Image = styled.img`
+    width: auto;
+`;
+
 const DetailsPane = styled.div`
     width: 50%;
     flex: 1;
+`;
+
+const QuantitySelector = styled.div`
+    display: flex;
 `;
 
 const ProductPage = () => {
@@ -32,9 +41,10 @@ const ProductPage = () => {
     const location = useLocation();
     const id = location.pathname.split("/")[2];
     const [product, setProduct] = useState([]);
+    const [quantity, setQuantity] = useState(1);
     
-      useEffect(() =>{
 
+      useEffect(() =>{
         const fetchProduct = () => {
             publicRequest.get("product/"+id).then(res =>{
               console.log(res);
@@ -45,21 +55,45 @@ const ProductPage = () => {
         fetchProduct();
       },[id])
 
+    
+    const handleQuantity = (type) => {
+        
+        if (type === "dec") {
+            quantity > 1 && setQuantity(quantity - 1);
+        } else {
+            quantity < product.availableQty ? setQuantity(quantity + 1) :
+            console.log("Not enough stock!");
+            
+        }
+        return quantity;
+    };
+    
+    
     return (
-
         <Container>
-
             <Navbar />
-
             <Wrapper>
                 <ImagesPane>
-                    <h2>Image comes here</h2>
+                    <Image src={product.imageUrl} alt="Product Image" />
                 </ImagesPane>
                 <DetailsPane>
                     <h1>{product.name}</h1>
+                    <p>{product.productCode}</p>
                     <p>{product.description}</p>
-                    <h2>{product.price}</h2>
+                    <p>{product.availableQty}</p>
+                    <h2>Rs. {product.price}</h2>
+
+                    <QuantitySelector>
+                        <Button onClick={() => handleQuantity("dec")}> - </Button>
+                        <p>{quantity}</p>
+                        <Button onClick={() => handleQuantity("inc")}> + </Button>
+                    </QuantitySelector>
+
+                    <Button>Add to cart</Button>
+                    
                 </DetailsPane>
+
+
             </Wrapper>
         </Container>
     )
